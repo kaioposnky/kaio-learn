eccb818a999e00be7a01bb90e6f966123331484d
Your Name <you@example.com>
2026-03-26 20:42:35 -0300

    [feat] conteúdo da aula

---
+++ soma10.pas
@@ -0,0 +1,17 @@
+program somar;
+var contador: integer;
+begin
+  contador := 0;
+  contador := contador + 1;
+  writeln(contador);
+
+(* Somar mais 1 *)
+
+(* Somar mais 2 *)
+
+(* Somar mais 3 *)
+
+(* Somar mais 4 *)
+
+(* Somar mais 5 *)
+end.
